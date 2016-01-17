69a744c3134cc17f694adb3a60102f54f33d9951
Steve Holdorf <holdorfsteve@gmail.com>
2016-01-17 12:07:24 -0500

    First Changes

---
+++ WebNewGet2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebNewGet2.Startup))]
+namespace WebNewGet2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
